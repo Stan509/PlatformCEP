af77b0d18cf24043371848a9ef017cd0f6f3d7cc
CEP Platform <noreply@cep.local>
2026-09-04 17:00:04 -0400

    fix(prod): production fixes for Django urls, docker compose port mapping and Rust base image

--- backend/django-core/config/urls.py
+++ backend/django-core/config/urls.py
@@ -1,4 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.http import JsonResponse
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 def root_api_index(request):
     return JsonResponse({
